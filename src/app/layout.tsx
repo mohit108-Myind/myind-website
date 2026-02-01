e74d35d36a4f91bc4262fc3b24bba1eabeecaa89
Mohit <mohit@Mohits-MacBook-Pro.local>
2026-02-01 13:25:23 +0530

    Add Apollo.io tracking script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Script from 'next/script'
 import "./globals.css";
 
 export const metadata = {
@@ -13,7 +14,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="bg-white text-neutral-900 antialiased">{children}</body>
+      <body className="bg-white text-neutral-900 antialiased">{children}
+        <Script id="apollo-tracking" strategy="afterInteractive">
+          {` function initApollo(){var n=Math.random().toString(36).substring(7),o=document.createElement("script");
+o.src="https://assets.apollo.io/micro/website-tracker/tracker.iife.js?nocache="+n,o.async=!0,o.defer=!0,
+o.onload=function(){window.trackingFunctions.onLoad({appId:"697349b6ac7a9000157ae147"})},
+document.head.appendChild(o)}initApollo();
+          `}
+        </Script>
+        
+      </body>
     </html>
   );
 }
